f1005c201c4e4e579653eb513d92fff4618a0e7c
SharletP <sharletman@gmail.com>
2019-08-21 15:33:16 +0300

    Updated NodeBELogger, v2 rewrite.

--- src/NodeBELogger.js
+++ src/NodeBELogger.js
@@ -1,31 +1,196 @@
+"use strict";
+
 const { Console } = require("console");
 
-const config = require("./configs/config.js");
-const colors = require("./modules/colors.js");
-
 const util = require("util");
-const moment = require("moment");
 
 class NodeBELogger extends Console {
-  constructor() {
-    super(process.stdout, process.stderr);
+  constructor(...args) {
+    super({
+      stdout: process.stdout,
+      stderr: process.stderr,
+      colors: true
+    });
     
+    this.config = require("./configs/config.js");
+    
+    this.colors = require("./modules/colors.js");
+    
+    this.def = true;
+        
     this.debugLevel = 0;
   }
   
-  setColor(clr) {
-    return util.format(colors.ESCAPE, clr);
+  /**
+   * Returns the set configuration.
+   *
+   * @return {object} config
+   */
+  
+  getConfig() {
+    return this.config;
   }
   
-  getTimestamp() {
-    return moment().format("YYYY-MM-DD HH:mm:ss");
+  /**
+   * Sets a specified value for given key in the configuration.
+   *
+   * @param {*} key
+   * @paran {*} value
+   *
+   */
+  
+  setConfig(key, value) {
+    this.config[key] = value;
   }
   
-  static parseData(data) {
+  /**
+   * Returns the debug level.
+   * 
+   * @return {integer} debugLevel
+   */  
+  
+  getDebugLevel() {
+    return this.debugLevel;
+  }
+  
+  /**
+   * Sets the specified debug level.
+   * 
+   * @param {integer} debugLevel
+   */
+  
+  setDebugLevel(debugLevel) {
+    this.debugLevel = debugLevel;
+  }
+  
+  /**
+   * Returns the color codes.
+   * 
+   * @return {object} colors
+   */
+  
+  getColors() {
+    return this.colors;
+  }
+  
+  /**
+   * Returns the current timestamp.
+   * 
+   * @return {string} timestamp
+   */
+  
+  get timestamp() {
+    return new Date().toISOString().replace("T", " ").substr(0, 19);
+  }
+  
+  /**
+   * Returns a bool to check if the given value is a color of colors.
+   * 
+   * @param {string} color
+   * @return {bool}
+   */
+  
+  isValidColor(color) {
+    for (let clr in this.getColors()) {
+      if (clr === color) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  /**
+   * Gets the requested color from colors.
+   * 
+   * @param {string} color
+   * @return {integer}
+   */
+  
+  getColor(color) {
+    if (this.isValidColor(color) !== true);
+    return this.colors[color];
+  }
+  
+  /**
+   * Returns the requested color from colors.
+   * 
+   * @param {string} color
+   * @return {}
+   */
+  
+  setColor(color) {
+    return util.format(this.colors["ESCAPE"], this.getColor(color));
+  }
+  
+  /**
+   * Gets the defined value for configuration logging.
+   * 
+   * @return {bool}
+   */
+  
+  getDef() {
+    return this.def;
+  }
+  
+  /**
+   * Sets the defined bool for configuration logging.
+   * 
+   * @param {bool} b
+   */
+  
+  setDef(b) {
+    this.def = b;
+  }
+  
+  /**
+   * Returns the parsed head format.
+   * 
+   * @param {string} dataType
+   * @param {string} color
+   * @param {bool} def
+   * @return {}
+   */
+  
+  headFormat(dataType, color, def = this.def) {
+    let frmt = this.config.format;
+      
+    return def === false ? frmt["head"](dataType, this.timestamp, this.setColor(frmt["colors"]["dataTypeColor"]), this.setColor(frmt["colors"]["timeStampColor"]), this.setColor("RESET"))
+    
+    :    
+    
+    `${this.setColor("BOLD_BRIGHT")}[ ${dataType} ]${this.setColor("RESET")} ${this.setColor("FG_DARK_GRAY")}( ${this.timestamp} )${this.setColor("RESET")}\n`;
+  }
+  
+  /**
+   * Returns the parsed body format.
+   * 
+   * @param {string} data
+   * @param {string} color
+   * @param {bool} def
+   * @return {}
+   */
+  
+  bodyFormat(data, color, def = this.def) {
+    return def === false ? this.config.format["body"](data, this.setColor(color), this.setColor("RESET"))
+    
+    :
+    
+    `>>> ${this.setColor(color)} ${data} ${this.setColor("RESET")}`;
+  }
+  
+  /**
+   * Returns the parsed in data as a string.
+   * 
+   * @param {*} data
+   * @return {string}
+   */
+  
+  _parseData(data) {
     if(typeof data === "string") {
       return data;
     } else if(Array.isArray(data)) {
-      return data.map(NodeBELogger.parseData).join("\n");
+      return data.map(this._parseData).join("\n");
     } else if(typeof data === "object" && data !== null) {
       return util.inspect(data, {
         depth: 0,
@@ -39,66 +204,66 @@ class NodeBELogger extends Console {
     return String(data);
   }
   
-  _writeLog(data, dataType = "LOG") {
-    data = NodeBELogger.parseData(data);
-    dataType = dataType.toUpperCase();
+  /**
+   * Writes the log with given parameters.
+   * 
+   * @param {string} data
+   * @param {string} color
+   * @param {string} dataType
+   */
+  
+  _writeLog(data, color, dataType) {
+    data = this._parseData(data);
     
-    const format = config.GLOBAL["format"];
-    
-    const time = config.GLOBAL["time"];
-    const { message } = config.GLOBAL[dataType];
-    
-    const timestamp = time(`(${this.getTimestamp()})`);
-    
-    const frmt = format(`[ ${dataType} ]`);
-    
-    super.log(`${frmt} ${timestamp}\n` + data.split("\n").map(str => `>>> ${message(str)}`).join("\n"));
+    super.log(`${this.headFormat(dataType)} ${this.bodyFormat(data, color)}`);
   }
   
-  info(...data) {
-    this._writeLog(data, "INFO");
+  /**
+   * Logs in the data from given parameters.
+   * 
+   * @param {string} data
+   * @param {string} color
+   * @param {string} dataType
+   *
+   * Types of In-Built Logs.
+   *  ~log, info, notice, alert, warning, emergency, critical, error & debug.
+   */
+  
+  log(data, color = "FG_LIGHT_GREEN", dataType = "LOG") {
+    this._writeLog(data, color, dataType);
   }
   
-  notice(...data) {
-    this._writeLog(data, "NOTICE");
+  info(data, color = "FG_WHITE", dataType = "INFO") {
+    this._writeLog(data, color, dataType);
   }
   
-  log(...data) {
-    this._writeLog(data, "LOG");
+  notice(data, color = "FG_LIGHT_CYAN", dataType = "NOTICE") {
+    this._writeLog(data, color, dataType);
   }
   
-  alert(...data) {
-    this._writeLog(data, "ALERT");
+  alert(data, color = "FG_LIGHT_RED", dataType = "ALERT") {
+    this._writeLog(data, color, dataType);
   }
   
-  warning(...data) {
-    this._writeLog(data, "WARNING");
+  warning(data, color = "FG_LIGHT_YELLOW", dataType = "WARNING") {
+    this._writeLog(data, color, dataType);
   }
   
-  error(...data) {
-    this._writeLog(data, "ERROR");
+  emergency(data, color = "FG_LIGHT_RED", dataType = "EMERGENCY") {
+    this._writeLog(data, color, dataType);
   }
   
-  emergency(...data) {
-    this._writeLog(data, "EMERGENCY");
-  }
-    
-  critical(...data) {
-    this._writeLog(data, "CRITICAL");
+  critical(data, color = "FG_LIGHT_RED", dataType = "CRITICAL") {
+    this._writeLog(data, color, dataType);
   }
   
-  debug(...data) {
-    if (this.debugLevel >= 1) {
-      this._writeLog(data, "DEBUG");
-    }
-  }
-    
-  getDebugLevel(v) {
-    return this.debugLevel;
+  error(data, color = "FG_RED", dataType = "ERROR") {
+    this._writeLog(data, color, dataType);
   }
   
-  setDebugLevel(v) {
-    this.debugLevel = v;
+  debug(data, color = "FG_DARK_GRAY", dataType = "DEBUG") {
+    if (!this.debugLevel >= 1) return;
+    this._writeLog(data, color, dataType);
   }
 }
 
